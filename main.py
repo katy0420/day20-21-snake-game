d299ce0bf9f8639c9ebc8381fed0fb628d5ba5af
Katy Tong <106995670+katy0420@users.noreply.github.com>
2022-06-09 18:11:52 -0400

    course 219

--- main.py
+++ main.py
@@ -34,13 +34,15 @@ while game_is_on:
 
     # TODO: collides with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() <-280:
-        game_is_on = False
-        scoreboard.game_over()
+        # game_is_on = False
+        scoreboard.reset()
+        snake.reset()
 
     # TODO: collides with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            # game_is_on = False
+            scoreboard.reset()
+            snake.reset()
 
 screen.exitonclick()
